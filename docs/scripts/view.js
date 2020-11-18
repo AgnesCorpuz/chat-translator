e318901df424b8e48400260d48cb3e9c1d57c191
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2020-11-18 17:56:05 +0800

    Redesign HTML

--- docs/scripts/view.js
+++ docs/scripts/view.js
@@ -25,32 +25,78 @@ export default {
     },
 
     displayLibraries(libraryId, libraryName){
-        var libContainer = document.createElement('div');
+        // var libContainer = document.createElement('div');
+        // libContainer.textContent = libraryName;
+        // libContainer.id = 'library-' + libraryId;
+        // libContainer.className = 'collapsible';
+
+        var libContainer = document.createElement('button');
         libContainer.textContent = libraryName;
         libContainer.id = 'library-' + libraryId;
         libContainer.className = 'collapsible';
+        libContainer.addEventListener('click', function() {
+            this.classList.toggle('active');
+            // var content = this.childNodes;
+            var content = this.nextElementSibling;
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
 
-        document.getElementById('libraries-container').appendChild(libContainer);
+        document.getElementById('canned-libraries-container').appendChild(libContainer);
     },
 
     displayResponses(response){
-        var responsesContainer = document.createElement('div');
-        responsesContainer.textContent = response.name;
-        responsesContainer.id = 'response-' + response.id;
-        responsesContainer.className = 'collapsible content';
+        // var responsesContainer = document.createElement('div');
+        // responsesContainer.textContent = response.name;
+        // responsesContainer.id = 'response-' + response.id;
+        // responsesContainer.className = 'collapsible content';
 
-        var contentContainer = document.createElement('div');
-        contentContainer.innerHTML = response.texts[0].content;
-        contentContainer.id = 'response-content-' + response.id;
-        contentContainer.className = 'content';
-        contentContainer.addEventListener('click', function() {
+        // var contentContainer = document.createElement('div');
+        // contentContainer.innerHTML = response.texts[0].content;
+        // contentContainer.id = 'response-content-' + response.id;
+        // contentContainer.className = 'content';
+        // contentContainer.addEventListener('click', function() {
+        //     // Add response in textarea
+        //     document.getElementById('message-textarea').innerHTML = response.texts[0].content;
+        //     window.location = 'index.html';
+        // });
+
+        // document.getElementById('library-' + response.libraries[0].id).appendChild(responsesContainer);
+        // document.getElementById('response-' + response.id).appendChild(contentContainer);
+
+        var responsesContainer = document.createElement('div');
+        responsesContainer.id = 'responses-container';
+        document.getElementById('canned-libraries-container').appendChild(responsesContainer);
+
+        var responseButton = document.createElement('button');
+        responseButton.textContent = response.name;
+        responseButton.id = 'response-' + response.id;
+        responseButton.className = 'content collapsible';
+        responseButton.addEventListener('click', function() {
+            this.classList.toggle('active');
+            // var content = this.childNodes;
+            var content = this.nextElementSibling;
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
+        document.getElementById('responses-container').appendChild(responseButton);
+
+        var responseText = document.createElement('p');
+        responseText.textContent = response.texts[0].content;;
+        responseText.id = 'response-content-' + response.id;
+        responseText.className = 'content';
+        responseText.addEventListener('click', function() {
             // Add response in textarea
             document.getElementById('message-textarea').innerHTML = response.texts[0].content;
             window.location = 'index.html';
         });
-
-        document.getElementById('library-' + response.libraries[0].id).appendChild(responsesContainer);
-        document.getElementById('response-' + response.id).appendChild(contentContainer);
+        document.getElementById('responses-container').appendChild(responseText);
     },
 
     addEventListeners(){
@@ -59,7 +105,8 @@ export default {
         for (var i = 0; i < element.length; i++) {
             element[i].addEventListener('click', function() {
                 this.classList.toggle('active');
-                var content = this.childNodes;
+                // var content = this.childNodes;
+                var content = this.nextElementSibling;
                 if (content.style.display === 'block') {
                     content.style.display = 'none';
                 } else {
