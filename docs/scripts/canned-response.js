f5cae29104558ce6d49bf03522128cd806f1e180
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2020-11-19 11:15:52 +0800

    getResponses bug fix

--- docs/scripts/canned-response.js
+++ docs/scripts/canned-response.js
@@ -3,23 +3,23 @@ import view from './view.js';
 const platformClient = require('platformClient');
 const responseManagementApi = new platformClient.ResponseManagementApi();
 
+function getResponses(libraryId, libraryName){
+    return responseManagementApi.getResponsemanagementResponses(libraryId)
+    .then((responses) => {
+        view.displayLibraries(libraryId, libraryName);
+
+        responses.entities.forEach((response) => {
+            view.displayResponses(response);
+        });
+    });
+}
+
 export default {
     getLibraries(){    
         return responseManagementApi.getResponsemanagementLibraries()
         .then((libraries) => {
             libraries.entities.forEach((library) => {
                 getResponses(library.id, library.name);
-            });
-        });
-    },
-
-    getResponses(libraryId, libraryName){
-        return responseManagementApi.getResponsemanagementResponses(libraryId)
-        .then((responses) => {
-            view.displayLibraries(libraryId, libraryName);
-    
-            responses.entities.forEach((response) => {
-                view.displayResponses(response);
             });
         });
     }
